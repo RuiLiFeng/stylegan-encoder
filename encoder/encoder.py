5da4eb952642fdb5ac8b583d1e5ccd9d2123841a
frl <frl1996@mail.ustc.edu.cn>
2019-08-03 21:40:56 +0800

    's'

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -88,9 +88,9 @@ def vae_encoder():
 
 def encoder_loss(G, E, D, E_opt, training_set, minibatch_size, reals, beta, labels=None, latent_broadcast=18):
     latents = E.get_output_for(reals, labels, is_training=True)
-    fakes = G.components.synthesis.run(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]),
-                                       minibatch_size=minibatch_size)
-    # fakes = G.components.synthesis.get_output_for(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]))
+    # fakes = G.components.synthesis.run(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]),
+    #                                    minibatch_size=minibatch_size)
+    fakes = G.components.synthesis.get_output_for(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]))
     v_loss = vgg_loss(training_set, reals, fakes)
     w_loss = wp_loss(D, reals, fakes, labels)
     loss = v_loss + beta * w_loss
