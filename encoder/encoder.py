48a24c2dd657b04f2124c4941afb4fd06a6ee37a
frl <frl1996@mail.ustc.edu.cn>
2019-08-03 21:37:53 +0800

    's'

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -9,17 +9,6 @@ from dnnlib.tflib.autosummary import autosummary
 from functools import partial
 
 
-def create_stub(name, batch_size):
-    return tf.constant(0, dtype='float32', shape=(batch_size, 0))
-
-
-def create_variable_for_generator(name, batch_size):
-    return tf.get_variable('learnable_dlatents',
-                           shape=(batch_size, 18, 512),
-                           dtype='float32',
-                           initializer=tf.initializers.random_normal())
-
-
 synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True), minibatch_size=8)
 
 # ----------------------------------------------------------------------------
@@ -100,9 +89,7 @@ def vae_encoder():
 def encoder_loss(G, E, D, E_opt, training_set, minibatch_size, reals, beta, labels=None, latent_broadcast=18):
     latents = E.get_output_for(reals, labels, is_training=True)
     fakes = G.components.synthesis.run(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]),
-                                       minibatch_size=minibatch_size,
-                                       custom_inputs=[partial(create_variable_for_generator, batch_size=minibatch_size),
-                                                      partial(create_stub, batch_size=minibatch_size)])
+                                       minibatch_size=minibatch_size)
     # fakes = G.components.synthesis.get_output_for(tf.tile(latents[:, np.newaxis], [1, latent_broadcast, 1]))
     v_loss = vgg_loss(training_set, reals, fakes)
     w_loss = wp_loss(D, reals, fakes, labels)
