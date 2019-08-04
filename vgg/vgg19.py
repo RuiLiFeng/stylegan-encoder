1337b6c97d6932a4abb8119e9eb49c9b1944cc6b
frl <frl1996@mail.ustc.edu.cn>
2019-08-04 23:14:24 +0800

    's'

--- vgg/vgg19.py
+++ vgg/vgg19.py
@@ -17,7 +17,7 @@ class Vgg19:
             vgg19_npy_path = path
             print(vgg19_npy_path)
         self.is_top = is_top
-        self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+        self.data_dict = np.load(vgg19_npy_path, allow_pickle=True, encoding='latin1').item()
         print("npy file loaded")
 
     def build(self, rgb):
